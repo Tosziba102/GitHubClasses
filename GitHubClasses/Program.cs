3ea570856b82487a8ca19e63dcabbc5bd25365dd
Dawid Laskowski <tosziba102pl@gmail.com>
2019-02-21 22:53:49 +0100

    Zmieniony string

--- GitHubClasses/Program.cs
+++ GitHubClasses/Program.cs
@@ -6,7 +6,7 @@ namespace GitHubClasses
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Dawid!");
         }
     }
 }
